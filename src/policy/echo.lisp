71d3a63397da0ee06f16bb590575bc93b47f8076
carrotflakes <carrotflakes@gmail.com>
2018-12-11 01:46:44 +0900

    add policy/echo

---
+++ src/policy/echo.lisp
@@ -0,0 +1,25 @@
+(defpackage schizoph.echo
+  (:use :cl
+        :schizoph.policy)
+  (:export :echo
+           :make-echo
+           :make-context))
+(in-package :schizoph.echo)
+
+(defclass echo (policy)
+  ())
+
+(defun make-echo ()
+  (make-instance 'echo))
+
+(defmethod make-context ((policy echo))
+  (make-instance 'context))
+
+(defmethod think ((policy echo) (intent t) (state state))
+  `((intent 1)))
+
+(defmethod next-context ((policy echo)
+                         (context context)
+                         (tactics t)
+                         (state state))
+  context)
